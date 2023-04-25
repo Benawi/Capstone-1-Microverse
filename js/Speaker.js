bad5e1f9619b817c8b574340eefef4290f52bf4f
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-04-25 21:05:29 +0300

    Add speaker data

---
+++ js/Speaker.js
@@ -0,0 +1,10 @@
+const speakerData=[
+    {
+        img: './img/posters/.jpg',
+        title: 'Fight Club',
+        author: 'David Fincher - 1999',
+        'film sinopsis':
+          '"An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more." — Imdb',
+      },
+]
+const mainSection = document.getElementById('main-section');
